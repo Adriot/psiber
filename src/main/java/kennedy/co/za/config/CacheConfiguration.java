a122b45ed0d3dfe2ba071a9827e7ff35120fee3d
Adroit_Mkhari <adriot.mkhari@fnb.co.za>
2020-02-25 20:45:30 +0200

    files generated after running 'jhipster import-jdl' for -> tax information jdl first template.

--- src/main/java/kennedy/co/za/config/CacheConfiguration.java
+++ src/main/java/kennedy/co/za/config/CacheConfiguration.java
@@ -42,6 +42,7 @@ public class CacheConfiguration {
             createCache(cm, kennedy.co.za.domain.User.class.getName());
             createCache(cm, kennedy.co.za.domain.Authority.class.getName());
             createCache(cm, kennedy.co.za.domain.User.class.getName() + ".authorities");
+            createCache(cm, kennedy.co.za.domain.TaxInformation.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
