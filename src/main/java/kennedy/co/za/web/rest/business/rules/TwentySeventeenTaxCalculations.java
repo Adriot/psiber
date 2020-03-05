2cc13fb723dd977b60406a8443a8b25bb59717db
Adroit_Mkhari <adriot.mkhari@fnb.co.za>
2020-03-05 09:43:12 +0200

    TwentyNineteenTaxCalculations to TwentySeventeenTaxCalculations

--- src/main/java/kennedy/co/za/web/rest/business/rules/TwentySeventeenTaxCalculations.java
+++ src/main/java/kennedy/co/za/web/rest/business/rules/TwentySeventeenTaxCalculations.java
@@ -2,8 +2,8 @@ package kennedy.co.za.web.rest.business.rules;
 
 import kennedy.co.za.service.dto.TaxInformationDTO;
 
-public class TwentyNineteenTaxCalculations extends TaxCalculations {
-    public TwentyNineteenTaxCalculations(TaxInformationDTO taxInformationDTO) {
+public class TwentySeventeenTaxCalculations extends TaxCalculations {
+    public TwentySeventeenTaxCalculations(TaxInformationDTO taxInformationDTO) {
         super(taxInformationDTO);
     }
 
